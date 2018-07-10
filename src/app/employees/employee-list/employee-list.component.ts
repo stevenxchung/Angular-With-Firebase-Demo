7be6d76d91d859f535dccb0628d4eb40870134d8
Steven <stevenxchung@gmail.com>
2018-07-10 12:57:25 -0400

    Add CRUD functionality to employee list component

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -2,16 +2,39 @@ import { Component, OnInit } from '@angular/core';
 
 // Inject EmployeeService to EmployeeListComponent class
 import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model'
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor(private employeeService: EmployeeService) { }
+  employeeList: Employee[];
+  constructor(private employeeService: EmployeeService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    let x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(element => {
+        let y = element.payload.toJSON();
+        y['$key'] = element.key;
+        this.employeeList.push(y as Employee);
+      });
+    });
+  }
+
+  onEdit(employee: Employee) {
+    // Use Object.assign() to make a copy of object (prevents modifying original data in real-time due to two-way-binding)
+    this.employeeService.selectedEmployee = Object.assign({}, employee);
+  }
+
+  onDelete(key: string) {
+    if (confirm("Are you sure you want to delete this record?") == true) {
+      this.employeeService.deleteEmployee(key);
+      this.toastr.warning('Deleted Successfully', 'Employee Register');
+    }
   }
 
 }
