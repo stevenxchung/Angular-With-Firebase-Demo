fc64cb20b874ac85bcb791fa8ccd6bdd63ea58b7
Steven <stevenxchung@gmail.com>
2018-07-10 18:02:56 -0400

    Add spaces

--- src/app/employees/shared/employee.service.ts
+++ src/app/employees/shared/employee.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Employee } from './employee.model';
 import { $ } from 'protractor';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class EmployeeService {
   employeeList: AngularFireList<any>;
   selectedEmployee: Employee = new Employee();
